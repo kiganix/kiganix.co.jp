426515d2c046c63f4c19eb4fa6152d5a51e8fbae
Shuma Yoshioka <shuma_yoshioka@voyagegroup.com>
2020-09-12 23:26:57 +0900

    Add analytics tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,6 +17,59 @@ const Layout  = ({ children }) => {
         {publicRuntimeConfig.otherLanguages.map((lng) => {
           return <link rel="alternate" hrefLang={lng} href={`${router.pathname}?lng=${lng}`}/>
         })}
+        <>
+            <script
+            dangerouslySetInnerHTML={{
+                __html: `
+!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='//static.ads-twitter.com/uwt.js',
+a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+// Insert Twitter Pixel ID and Standard Event data below
+twq('init','o2yr9');
+twq('track','PageView');
+                `
+            }}/>
+        </>
+        <>
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '2488404988044456');
+fbq('track', 'PageView');
+                    `
+                }}/>
+            <noscript>
+                <img
+                    height="1"
+                    width="1"
+                    style={{ display: 'none' }}
+                    src="https://www.facebook.com/tr?id=2488404988044456&ev=PageView&noscript=1"/>
+            </noscript>
+        </>
+        <>
+            <script
+                async={true}
+                src="https://www.googletagmanager.com/gtag/js?id=UA-156087543-1"/>
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'AW-675260250');
+gtag('config', 'UA-156087543-1');
+                    `
+                }}/>
+        </>
       </Head>
       <div>
         {children}
