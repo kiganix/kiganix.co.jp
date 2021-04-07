e712c0720d5f3d8f035fdaa93b32af8eb05d5a3c
Shuma Yoshioka <shuma_yoshioka@kiganix.co.jp>
2021-04-07 23:08:56 +0900

    Tweak swe

--- pages/services/index.tsx
+++ pages/services/index.tsx
@@ -16,9 +16,14 @@ function ServicesIndex(props: Props) {
             <h1>ソフトウェア受託開発事業</h1>
             <>
                 創業者である吉岡 秀馬の個人事業を継承し、フリーランスとしてSESに相当する受託開発事業を展開しています。<br/>
-                主にスタートアップ企業を対象とし、新規事業開発において求められる高度なソフトウェアの設計・開発を得意としています。
+                主にスタートアップ企業を対象とし、新規事業開発において求められる高度なソフトウェアの設計・開発を得意としています。<br/>
+                また事業の持続可能高める事を目的とし、利用しているオープンソースソフトウェアへの金銭的支援や、自社開発製品のオープンソース化を推進しています。
                 <ul>
                     <li><a href="https://lapras.com/public/AQNWBZV" target="_blank">吉岡 秀馬 | LAPRAS Profile</a></li>
+                    <li><a href="https://kiga.dev" target="_blank">kiga.dev</a></li>
+                    <li><a href="https://nfcuid.kiganix.com" target="_blank">NFC UID Reader</a></li>
+                    <li><a href="https://github.com/kiganix" target="_blank">GitHub (Kiganix)</a></li>
+                    <li><a href="https://opencollective.com/kiganix" target="_blank">Open Collective (Kiganix)</a></li>
                 </ul>
             </>
         </Section>
