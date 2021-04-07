f4567c872723c439a4ede29191c140b4bfd0c70d
Shuma Yoshioka <shuma_yoshioka@kiganix.co.jp>
2021-04-07 21:42:48 +0900

    Tweak table style

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faQuestionCircle } from '@fortawesome/free-regular-svg-icons'
 import { shuffle } from 'd3-array'
 import { TFunction, WithTranslation } from 'next-i18next'
+import { useTranslation } from 'react-i18next'
 import { GetServerSideProps, NextPageContext } from 'next'
 import Link from 'next/link'
 import { Section } from '../components/about/Section'
@@ -51,7 +52,78 @@ function AboutKiganix() {
             src="https://kiganix.imgix.net/static/penguin.png?w=800&amp;auto=compress"/>
         合同会社キガニックスは、東京都昭島市を拠点とするテクノロジー企業です。<br/>
         ソフトウェア技術をレバレッジに効率化を推し進め、様々な関連事業を企画・展開しています。
+        <IndexTable/>
     </Section>
+}
+
+function IndexTable() {
+    const { t } = useTranslation()
+
+    return <table className={css.details}>
+        <tbody>
+            <tr>
+                <th>{t('label-for-company-legal-name')}</th>
+                <td>{t('company-legal-name')}</td>
+            </tr>
+            <tr>
+                <th>{t('label-for-company-number')}</th>
+                <td>
+                    <a href={t('nta-company-number')} target="_blank">8012803002172</a>
+                    <a href={t('about-nta-company-number')} target="_blank"><FontAwesomeIcon icon={faQuestionCircle} className={css.infomark}/></a>
+                </td>
+            </tr>
+            <tr>
+                <th>D-U-N-S® Number</th>
+                <td>
+                    <a href="https://www.dnb.com/business-directory/company-profiles.kiganix_gk.f60e4dc6c2c6e86e360766f860bc0950.html" target="_blank">718232158</a>
+                    <a href={t('about-duns-number')} target="_blank"><FontAwesomeIcon icon={faQuestionCircle} className={css.infomark}/></a>
+                </td>
+            </tr>
+            <tr>
+                <th>GS1 Company Prefix</th>
+                <td>
+                    <span>457151931</span>
+                    <a href={t('about-gs1prefix')} target="_blank"><FontAwesomeIcon icon={faQuestionCircle} className={css.infomark}/></a>
+                </td>
+            </tr>
+            <tr>
+                <th>GLN</th>
+                <td>
+                    <span>4571519310004</span>
+                    <a href={t('about-gln')} target="_blank"><FontAwesomeIcon icon={faQuestionCircle} className={css.infomark}/></a>
+                </td>
+            </tr>
+            <tr>
+                <th>{t('label-for-corporate-website')}</th>
+                <td>
+                    <span>https://kiganix.co.jp</span>
+                    <a href={t('about-organizational-jp-domain')} target="_blank"><FontAwesomeIcon icon={faQuestionCircle} className={css.infomark}/></a>
+                </td>
+            </tr>
+            <tr>
+                <th>{t('label-for-foundation-date')}</th>
+                <td>{t('foundation-date')}</td>
+            </tr>
+            <tr>
+                <th>{t('label-for-ceo')}</th>
+                <td><a href="https://github.com/S64" target="_blank">{t('ceo')}</a></td>
+            </tr>
+            <tr>
+                <th>{t('label-for-our-business')}</th>
+                <td>
+                    <ul>
+                        <li><a href="https://lapras.com/public/AQNWBZV" target="_blank">{t('entrusted-software-development')}</a></li>
+                        <li><a href="https://seminar.kiganix.com" target="_blank">{t('educational-business')}</a></li>
+                        <li><a href="https://music.kiganix.com" target="_blank">{t('music-publishing')}</a></li>
+                    </ul>
+                </td>
+            </tr>
+            <tr>
+                <th>{t('label-for-newsletter')}</th>
+                <td><a href="https://newsletter.kiganix.com" target="_blank">Kiganix Newsletter</a></td>
+            </tr>
+        </tbody>
+    </table>
 }
 
 const Index = ({ t, i18n, shuffledClients }: Props) => (
@@ -59,75 +131,8 @@ const Index = ({ t, i18n, shuffledClients }: Props) => (
         <Head>
             <title lang={i18n.language}>{t('title')}</title>
         </Head>
-        <>
-            <AboutKiganix/>
-        </>
+        <AboutKiganix/>
         <div className={css.index}>
-            <table className={css.details}>
-                <tbody>
-                    <tr>
-                        <th>{t('label-for-company-legal-name')}</th>
-                        <td>{t('company-legal-name')}</td>
-                    </tr>
-                    <tr>
-                        <th>{t('label-for-company-number')}</th>
-                        <td>
-                            <a href={t('nta-company-number')} target="_blank">8012803002172</a>
-                            <a href={t('about-nta-company-number')} target="_blank"><FontAwesomeIcon icon={faQuestionCircle} className={css.infomark}/></a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>D-U-N-S® Number</th>
-                        <td>
-                            <a href="https://www.dnb.com/business-directory/company-profiles.kiganix_gk.f60e4dc6c2c6e86e360766f860bc0950.html" target="_blank">718232158</a>
-                            <a href={t('about-duns-number')} target="_blank"><FontAwesomeIcon icon={faQuestionCircle} className={css.infomark}/></a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>GS1 Company Prefix</th>
-                        <td>
-                            <span>457151931</span>
-                            <a href={t('about-gs1prefix')} target="_blank"><FontAwesomeIcon icon={faQuestionCircle} className={css.infomark}/></a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>Global Location Number (GLN)</th>
-                        <td>
-                            <span>4571519310004</span>
-                            <a href={t('about-gln')} target="_blank"><FontAwesomeIcon icon={faQuestionCircle} className={css.infomark}/></a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>{t('label-for-corporate-website')}</th>
-                        <td>
-                            <span>https://kiganix.co.jp</span>
-                            <a href={t('about-organizational-jp-domain')} target="_blank"><FontAwesomeIcon icon={faQuestionCircle} className={css.infomark}/></a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>{t('label-for-foundation-date')}</th>
-                        <td>{t('foundation-date')}</td>
-                    </tr>
-                    <tr>
-                        <th>{t('label-for-ceo')}</th>
-                        <td><a href="https://github.com/S64" target="_blank">{t('ceo')}</a></td>
-                    </tr>
-                    <tr>
-                        <th>{t('label-for-our-business')}</th>
-                        <td>
-                            <ul>
-                                <li><a href="https://lapras.com/public/AQNWBZV" target="_blank">{t('entrusted-software-development')}</a></li>
-                                <li><a href="https://seminar.kiganix.com" target="_blank">{t('educational-business')}</a></li>
-                                <li><a href="https://music.kiganix.com" target="_blank">{t('music-publishing')}</a></li>
-                            </ul>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>{t('label-for-newsletter')}</th>
-                        <td><a href="https://newsletter.kiganix.com" target="_blank">Kiganix Newsletter</a></td>
-                    </tr>
-                </tbody>
-            </table>
             <h2>{t('our-clients')}</h2>
             <ul>
                 { shuffledClients.map((itr) => <ClientListItem key={itr.nameKey} client={itr} t={t} />) }
